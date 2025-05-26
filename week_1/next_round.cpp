108b443bf811e7c1b823009e5cb859cb2da02401
Himanshu Thalor <24b0933@iitb.ac.in>
2025-05-27 00:14:41 +0530

    Add Week 1 solutions

--- week_1/next_round.cpp
+++ week_1/next_round.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n , k ;
-    cin>>n>>k;
-    int count =0;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int count = 0;
     int a[n];
     for (int i = 0; i < n; i++)
-    { cin>>a[i];
-     }
-    for(int i = 0; i < n; i++)
-    { if(a[i]>0){
-        if(a[i]>=a[k-1]){count++;}
+    {
+        cin >> a[i];
     }
-     }
-    
-    cout<<count;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+        {
+            if (a[i] >= a[k - 1])
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count;
 }
